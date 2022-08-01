d7c19c052dbcb37e2390ec0919b7b195dcc5e655
Hira Khan <phr.hirakhan@gmail.com>
2022-08-01 18:14:23 +0200

    made changes

--- titus/views.py
+++ titus/views.py
@@ -9,10 +9,11 @@ def example_view(request):
 
 
 def variable_view(request):
-    my_var = {'first_name': 'AnNe',
+    my_var = {'first_name': 'ANNE',
               'last_name': 'FrAnk',
               'some_list': [1, 2, 3],
-              'some_dict': {'inside_key': 'inside_value'}}
+              'some_dict': {'inside_key': 'inside_value'},
+              'user_logged_in': False}
 
 
     return render(request, 'titus/variable.html', context=my_var)
