22dbc874e548ae1244d5d6fa3d210f81dee462d6
Hira Khan <phr.hirakhan@gmail.com>
2022-08-01 11:57:28 +0200

    initial commit

---
+++ titus/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def example_view(request):
+    # titus/templates/titus/example.html
+    return render(request, 'titus/example.html')
+
+
+def variable_view(request):
+    my_var = {'first_name': 'AnNe',
+              'last_name': 'FrAnk',
+              'some_list': [1, 2, 3],
+              'some_dict': {'inside_key': 'inside_value'}}
+
+
+    return render(request, 'titus/variable.html', context=my_var)
